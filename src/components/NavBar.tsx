d59f9cde7dbc12c57f0ac36eb0a9055c60ad8be6
Iftikhar Ahmed <iffi_kunjahi@yahoo.com>
2024-10-06 11:34:01 +0200

    Logo component created, having color mode effect

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBox from "./SearchBox";
+import Logo from "./Logo";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between">
       <HStack alignItems="center" gap={0}>
-        <Image src={logo} boxSize="60px"></Image>   
+        <Logo />   
         <Text fontSize="3xl" fontWeight="bold">J.RECIPES</Text>
       </HStack>
       <SearchBox />   
