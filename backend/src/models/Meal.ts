0662525e28b6b441f39d3ce93a0d33880f50c8d0
Iftikhar Ahmed <iffi_kunjahi@yahoo.com>
2024-11-13 10:33:58 +0100

    Typescript setup in backend and code changed to typescript compatible

---
+++ backend/src/models/Meal.ts
@@ -0,0 +1,30 @@
+// models/Meal.ts
+import mongoose, { Schema, Document, Types } from 'mongoose'
+import { IUser } from './User';
+
+interface IMeal extends Document {
+  name: string;
+  category: string;
+  ingredients: string[];
+  tags: string[];
+  area: string;
+  youtubeLink: string;
+  image: string;
+  description: string;
+  user: Types.ObjectId | IUser;
+}
+
+const mealSchema: Schema<IMeal> = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  ingredients: { type: [String], required: false },
+  tags: { type: [String], required: false },
+  area: { type: String, required: false },
+  youtubeLink: { type: String, required: false },
+  image: { type: String, required: false },
+  description: { type: String, required: false },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+const Meal = mongoose.model<IMeal>('Meal', mealSchema)
+export default Meal
---
+++ backend/src/models/User.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+// Define the User interface
+interface IUser extends Document {
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+  comparePassword(password: string): Promise<boolean>;
+}
+
+// Define the User schema
+const userSchema: Schema<IUser> = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+// Hash the password before saving the user document
+userSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare passwords during login
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export { User, IUser }
