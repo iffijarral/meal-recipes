593d08074adf033510601a1bade527a85f840380
Iftikhar Ahmed <iffi_kunjahi@yahoo.com>
2024-11-19 20:01:23 +0100

    validation, verification, emailSend added

---
+++ backend/src/validators/userValidator.ts
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+// User validation schema using Joi
+export const userSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+  .min(8)
+  .max(30)
+  .pattern(new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&*!])'))
+  .required()
+  .messages({
+    'string.pattern.base': 'Password must include at least one lowercase letter, one uppercase letter, one digit, and one special character.',
+  }),
+  role: Joi.string().required(),
+  isActive: Joi.boolean().required(),
+  isVerified: Joi.boolean().required()
+});
